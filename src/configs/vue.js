f038030b60e9dc2e699664c5415dc8049e8af8af
Eduard Aksamitov <e@euaaaio.ru>
2025-05-30 15:19:13 +0300

    feat: add `vue/no-unused-emit-declarations`

--- src/configs/vue.js
+++ src/configs/vue.js
@@ -48,6 +48,7 @@ export function vue(options = {}) {
 				'vue/no-undef-components': ['error', {
 					ignorePatterns: ['RouterLink'],
 				}],
+				'vue/no-unused-emit-declarations': ['error'],
 				'vue/no-v-html': 'off',
 				'vue/max-attributes-per-line': 'off',
 				'vue/singleline-html-element-content-newline': 'off',
