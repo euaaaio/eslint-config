01033db0eaa6119ce337f47eedbbd7e29cf6dd43
Eduard Aksamitov <e@euaaaio.ru>
2025-05-30 15:43:10 +0300

    feat(ts): disable not stable strict type rules

--- src/configs/typescript.js
+++ src/configs/typescript.js
@@ -43,8 +43,13 @@ export function typescript(options = {}) {
 				'@typescript-eslint/restrict-template-expressions': 'off',
 				'@typescript-eslint/prefer-nullish-coalescing': 'off',
 				'@typescript-eslint/no-empty-function': 'off',
+
+				// Disable not stable
 				'@typescript-eslint/no-unsafe-argument': 'off',
+				'@typescript-eslint/no-unsafe-assignment': 'off',
 				'@typescript-eslint/no-unsafe-member-access': 'off',
+				'@typescript-eslint/no-unsafe-return': 'off',
+				'@typescript-eslint/no-unsafe-call': 'off',
 
 				// Improvements
 				'@typescript-eslint/array-type': ['error', {
