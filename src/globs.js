a56c02a383697a82c8b6b92e902be6b5310cdfb2
Eduard Aksamitov <e@euaaaio.ru>
2025-06-27 16:43:25 +0300

    feat(astro/unicorn): use `camelCase` filename case for pages

--- src/globs.js
+++ src/globs.js
@@ -4,6 +4,7 @@ export const GLOB_TS = '**/*.ts'
 export const GLOB_DTS = '**/*.d.ts'
 export const GLOB_VUE = '**/*.vue'
 export const GLOB_ASTRO = '**/*.astro'
+export const GLOB_ASTRO_PAGES = '**/pages/**/*.astro'
 
 export const GLOB_STORYBOOK = '**/*.stories.{js,ts}'
 
